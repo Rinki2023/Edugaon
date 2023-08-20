9827892b9fea3a338db36ec624304ba6f9ddac6a
RinkiKumari <121715851+Rinki2023@users.noreply.github.com>
2023-08-20 10:11:17 +0530

    edugaon

---
+++ precache-manifest.bcdacb7dd14db113f3c8e409fc47d45e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "34baf7a07aee149b4962",
+    "url": "/static/css/main.6b6f7371.chunk.css"
+  },
+  {
+    "revision": "34baf7a07aee149b4962",
+    "url": "/static/js/main.34baf7a0.chunk.js"
+  },
+  {
+    "revision": "067be1fa11608c35988c",
+    "url": "/static/js/1.067be1fa.chunk.js"
+  },
+  {
+    "revision": "d64fc31001d878c2c325",
+    "url": "/static/js/2.d64fc310.chunk.js"
+  },
+  {
+    "revision": "4cca54895c0f3b6e4246",
+    "url": "/static/js/runtime~main.4cca5489.js"
+  },
+  {
+    "revision": "c68b519e16418b7c0dc5e6916fff6919",
+    "url": "/index.html"
+  }
+];
